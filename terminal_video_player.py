8f8ee5866e6a31223acd0763add2494d8a31dfd6
Roman Lisov <lupusarqentum@protonmail.com>
2024-09-08 06:44:34 +0500

    Check if main script is actually executed as a scirpt

--- terminal_video_player.py
+++ terminal_video_player.py
@@ -18,25 +18,26 @@ def get_terminal_size() -> tuple:
     return int(rows), int(columns)
 
 
-CONFIG_LOCATION_PREFIX = "./"
-config = Configuration()
-if not config.read_and_apply_JSON_config(CONFIG_LOCATION_PREFIX + "config.json"):
-    print_error("An error occurred when trying to apply config. Fallback to default config instead")
-    if not config.read_and_apply_JSON_config(CONFIG_LOCATION_PREFIX + "default_config.json"):
-        print_error("An error occurred when trying to apply default config. Can't operate")
-        sys.exit(-1)
+if __name__ == "__main__":
+    CONFIG_LOCATION_PREFIX = "./"
+    config = Configuration()
+    if not config.read_and_apply_JSON_config(CONFIG_LOCATION_PREFIX + "config.json"):
+        print_error("An error occurred when trying to apply config. Fallback to default config instead")
+        if not config.read_and_apply_JSON_config(CONFIG_LOCATION_PREFIX + "default_config.json"):
+            print_error("An error occurred when trying to apply default config. Can't operate")
+            sys.exit(-1)
 
-target_file_path = "examples/input.png"
-terminal_rows, terminal_columns = get_terminal_size()
+    target_file_path = "examples/input.png"
+    terminal_rows, terminal_columns = get_terminal_size()
 
-image_processor = ImageProcessor(config)
-image_processor.load(target_file_path)
-image_processor.render(terminal_rows, terminal_columns)
+    image_processor = ImageProcessor(config)
+    image_processor.load(target_file_path)
+    image_processor.render(terminal_rows, terminal_columns)
 
-if config.get_boldify():
-    print("\033[1m")
-    image_processor.display()
-    print("\033[0m")
-else:
-    image_processor.display()
+    if config.get_boldify():
+        print("\033[1m")
+        image_processor.display()
+        print("\033[0m")
+    else:
+        image_processor.display()
 
