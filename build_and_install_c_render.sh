4cfc8f27b4526488eeea5793078aba4276330518
Roman Lisov <lupusarqentum@protonmail.com>
2024-09-14 19:10:34 +0500

    move build script to the root directory

---
+++ build_and_install_c_render.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+PYTHON_INTERPRETER_INCLUDE_DIR=/usr/include/python3.12
+NUMPY_INCLUDE_DIR=venv/lib/python3.12/site-packages/numpy/_core/include
+INSTALLATION_DIR=venv/lib/python3.12/site-packages
+
+PYTHON_INTERPRETER_INCLUDE_DIR=$(realpath $PYTHON_INTERPRETER_INCLUDE_DIR)
+NUMPY_INCLUDE_DIR=$(realpath $NUMPY_INCLUDE_DIR)
+INSTALLATION_DIR=$(realpath $INSTALLATION_DIR)
+
+cd src/c
+
+echo Compiling extension module...
+
+gcc -fPIC -c -I"$PYTHON_INTERPRETER_INCLUDE_DIR" -I"$NUMPY_INCLUDE_DIR" terminalrenderer_module.c render.c
+
+echo Linking shared library...
+
+gcc --shared terminalrenderer_module.o render.o -o terminalrenderer.so
+
+echo Installing...
+
+cp terminalrenderer.so "$INSTALLATION_DIR"
+
+echo Cleaning...
+
+rm -f terminalrenderer.so
+rm -f terminalrenderer_module.o
+rm -f render.o
